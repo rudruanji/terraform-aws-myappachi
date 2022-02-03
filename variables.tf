2b926eaa5f427bf9e31735e97a9131861ab00e33
ruruanji <rudruanji.sruthi@gmail.com>
2022-02-03 06:46:00 +0530

    terraform modile

---
+++ variables.tf
@@ -0,0 +1,25 @@
+variable "region" {
+  default = "us-east-2"
+}
+
+variable "accesskey" {
+	default="" 
+}
+
+variable "secretkey" {
+	default=""
+}
+
+variable "vpcID" {
+	default="vpc-0eb0c00fb0e8cded4"
+}
+variable "amiID" {
+	default="ami-0ba62214afa52bec7"
+}
+variable "keyPair" {
+	default="myohiokeys"
+}
+
+
+
+
